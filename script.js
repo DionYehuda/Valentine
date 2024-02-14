52f42d95a8f47215bcd32b894811d864d52178f1
Dion Yehuda <57145604+DionYehuda@users.noreply.github.com>
2024-02-14 09:14:34 +0700

    yes is done

--- script.js
+++ script.js
@@ -14,7 +14,14 @@ var heartisclicked = false;
 var i = 0;
 var usedtext = "";
 const text1 = "Would you be my valentine??";
-
+const yestext1 = "Reaalllyyyy??? omagaaa";
+const yestext2 = "I wanna tell you something";
+const yestext3 = "For me, every day with you is Valentine’s Day."; 
+const yestext4 = "Your smile sets my heart on fire."
+const yestext5 = "With every passing moment, my heart is drawn to the warmth of your love"
+const yestext6 = "Words can’t describe my love for you, so I made you this!"
+const yestext7 = "Thank you for being my Valentine."
+const yestext8 = "I Love You."
 
 
 
@@ -57,14 +64,119 @@ function showbutton(){
     yesbtn.className = "yes-btn";
     yesbtn.innerHTML = "Yes";
     yesbtn.addEventListener("click", () =>{
-        question.innerHTML = "Aaaaa, I like you too";
         btngroup.classList.remove("fadein");
         btngroup.classList.add("fadeout");
         gificon.classList.remove("fadein");
         gificon.classList.add("fadeout");
+        question.classList.remove("fadein");
+        question.classList.add("fadeout");
         setTimeout(() => {
             btngroup.classList.remove("fadeout");
             btngroup.classList.add("hidden");
+            gificon.src = "https://raw.githubusercontent.com/DzarelDeveloper/Img/main/gif.webp";    
+            gificon.classList.remove("fadeout");
+            gificon.classList.add("fadein");
+            question.innerHTML = yestext1;
+            question.classList.remove("fadeout");
+            question.classList.add("fadein");
+            setTimeout(() => {
+                gificon.classList.remove("fadein");
+                gificon.classList.add("fadeout");
+                question.classList.remove("fadein");
+                question.classList.add("fadeout");
+                setTimeout(() => {
+                    gificon.src = "src/angry-animated-bears-9z416raof8j7etdu.webp";    
+                    gificon.classList.remove("fadeout");
+                    gificon.classList.add("fadein");
+                    question.innerHTML = yestext2;
+                    question.classList.remove("fadeout");
+                    question.classList.add("fadein");
+                    setTimeout(() => {
+                        gificon.classList.remove("fadein");
+                        gificon.classList.add("fadeout");
+                        question.classList.remove("fadein");
+                        question.classList.add("fadeout");
+                        setTimeout(() => {
+                            gificon.src = "src/Kiss.gif";    
+                            gificon.classList.remove("fadeout");
+                            gificon.classList.add("fadein");
+                            question.innerHTML = yestext3;
+                            question.classList.remove("fadeout");
+                            question.classList.add("fadein");
+                            setTimeout(() => {
+                                gificon.classList.remove("fadein");
+                                gificon.classList.add("fadeout");
+                                question.classList.remove("fadein");
+                                question.classList.add("fadeout");
+                                setTimeout(() => {
+                                    gificon.src = "src/smile-milk.gif";    
+                                    gificon.classList.remove("fadeout");
+                                    gificon.classList.add("fadein");
+                                    question.innerHTML = yestext4;
+                                    question.classList.remove("fadeout");
+                                    question.classList.add("fadein");
+                                    setTimeout(() => {
+                                        gificon.classList.remove("fadein");
+                                        gificon.classList.add("fadeout");
+                                        question.classList.remove("fadein");
+                                        question.classList.add("fadeout");
+                                        setTimeout(() => {
+                                            gificon.src = "src/milk-and-mocha-icegif1.gif"; 
+                                            gificon.classList.remove("fadeout");
+                                            gificon.classList.add("fadein");
+                                            question.innerHTML = yestext5;
+                                            question.classList.remove("fadeout");
+                                            question.classList.add("fadein");
+                                            setTimeout(() => {
+                                                gificon.classList.remove("fadein");
+                                                gificon.classList.add("fadeout");
+                                                question.classList.remove("fadein");
+                                                question.classList.add("fadeout");
+                                                setTimeout(() => {
+                                                    gificon.src = "src/Work.gif";    
+                                                    gificon.classList.remove("fadeout");
+                                                    gificon.classList.add("fadein");
+                                                    question.innerHTML = yestext6;
+                                                    question.classList.remove("fadeout");
+                                                    question.classList.add("fadein");
+                                                    setTimeout(() => {
+                                                        gificon.classList.remove("fadein");
+                                                        gificon.classList.add("fadeout");
+                                                        question.classList.remove("fadein");
+                                                        question.classList.add("fadeout");
+                                                        setTimeout(() => {
+                                                            gificon.src = "src/love-milk-mocha-kiss-grukv7pst0hcyojf.gif";    
+                                                            gificon.classList.remove("fadeout");
+                                                            gificon.classList.add("fadein");
+                                                            question.innerHTML = yestext7;
+                                                            question.classList.remove("fadeout");
+                                                            question.classList.add("fadein");
+                                                            setTimeout(() => {
+                                                                gificon.classList.remove("fadein");
+                                                                gificon.classList.add("fadeout");
+                                                                question.classList.remove("fadein");
+                                                                question.classList.add("fadeout");
+                                                                setTimeout(() => {
+                                                                    gificon.src = "src/eccomi.gif";    
+                                                                    gificon.classList.remove("fadeout");
+                                                                    gificon.classList.add("fadein");
+                                                                    question.innerHTML = yestext8;
+                                                                    question.classList.remove("fadeout");
+                                                                    question.classList.add("fadein");
+                                                                }, 3000);
+                                                            }, 5000);
+                                                        }, 3000);
+                                                    }, 5000);
+                                                }, 3000);
+                                            }, 5000);
+                                        }, 3000);
+                                    }, 5000);
+                                }, 3000);
+                            }, 5000);
+                        }, 3000);
+                    }, 5000);
+                }, 3000);
+            }, 5000);
         }, 3000);
         yesbtn.addEventListener("click", () => {
             question.innerHTML = "Hehehe";
